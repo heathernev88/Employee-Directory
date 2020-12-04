61a4c053c4ee509af3797e2236dfdbbaf8c746d1
HeatherNev88 <heather.alv88@yahoo.com>
2020-12-04 15:10:17 -0800

    components created, app.js updated to add componenets

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h2>Copyright Heather Nevarez</h2>
+    </div>
+  );
+};
+
+export default Footer;
